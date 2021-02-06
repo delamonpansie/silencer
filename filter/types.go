59bc75f52bc0042b65e78271dc9bcd86d58c0911
Yury Vostrikov <mon@unformed.ru>
2021-02-06 20:43:23 +0100

    Switch mockget to source mode
    It's dump and less fragile

--- filter/types.go
+++ filter/types.go
@@ -1,6 +1,7 @@
 package filter
 
-//go:generate mockgen -destination types_mock.go -package filter . Blocker
+//go:generate mockgen -destination types_mock.go -package filter -source types.go Blocker
+
 type Blocker interface {
 	Block(string)
 	Unblock(string)
